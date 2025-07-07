1887eedf80c7ae288d64ad212f2de5b051ccdcd8
tiwnehakumari <neha.hestabit@gmail.com>
2025-07-07 17:02:55 +0530

    added first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header"
+function App(){
+  return(
+    <div>
+      <Header/>
+      <h1>hello React</h1> 
+      <h1>hello React</h1> 
+    </div>
+   
+  )
+  
+}
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+function Header(){
+    return(
+        <div>
+            <h1>Header 1</h1>
+            <h1>Header 2</h1>
+            <h1>Header 3</h1>
+        </div>
+    )
+}
+export default Header
